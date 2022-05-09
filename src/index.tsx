a6d2b8a9e284a42e33727fe312090b57e39d8d10
engsekine <engsekine@gmail.com>
2022-05-09 21:19:02 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ console.log("./src/index.tsx");
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { App } from './App';
-
+import "./scss/style.scss";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
